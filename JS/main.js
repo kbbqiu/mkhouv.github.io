fe817a459e77ef4dbb590fa9a2710b17068ae138
Michael Khouv <Michael@Michaels-MacBook-Pro.local>
2017-02-15 00:26:47 -0800

    Testing

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 // Create our 'main' state that will contain the game
 var currentUser = firebase.database().ref("/messages/" + window.fireID);
 var Messages = firebase.database().ref("/messages");
-console.log("test20");
+console.log("test22");
         
 var stars;
 var highScore = 0;
@@ -71,14 +71,14 @@ var mainState = {
         if(highScore < this.score){
             highScore = this.score;
             var currentHighScore;
-            currentUser.on("value", function(data){
-                console.log(data.val());
-                if (highScore < data.val().score) {
-                    highScore = data.val().score;
-                    $("#highscore").text("YOUR HIGH SCORE: " + data.val().score);
-                    currentUser.update({
-                        score: highScore
-                    })
+            currentUser.on("value", function(score){
+                console.log(score.val())
+//                if (highScore < score.val().score) {
+//                    highScore = score.val().score;
+//                    $("#highscore").text("YOUR HIGH SCORE: " + score.val().score);
+//                    currentUser.update({
+//                        score: highScore
+//                    })
                 }
             })
             Messages.on("value", function(data) {
