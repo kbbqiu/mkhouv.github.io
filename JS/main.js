67385f9858f76faebdd5d3a7657ac0ad36be6aad
Michael Khouv <Michael@Michaels-MacBook-Pro.local>
2017-02-14 23:10:14 -0800

    Testing

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 // Create our 'main' state that will contain the game
 var Messages = firebase.database().ref('/messages')
-console.log("test3")
+console.log("test4")
         
 var stars;
 var highScore = 0;
